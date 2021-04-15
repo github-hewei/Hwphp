90c3c93e91a8d0142e6dc4bb394c9cc0fe1e0928
hewei <hemailads@163.com>
2021-04-15 21:40:23 +0800

    curl library

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 ```sh
 composer require hwphp/hwphp
 ```
+
+```php
+require_once 'vendor/autoload.php';
+use Hwphp\Curl;
+use Hwphp\curl\Exception as CurlException;
+
+$curl = new Curl();
+
+
+```
---
+++ src/curl/Exception.php
@@ -0,0 +1,35 @@
+<?php //CODE BY HW
+namespace Hwphp\curl;
+
+/**
+ * cURL异常处理类
+ */
+class Exception extends \Exception {
+
+    /**
+     * cURL错误编号
+     * @var int
+     */
+    public $curl_errorno;
+
+    /**
+     * cURLException constructor.
+     * @param string $message 异常信息
+     * @param int $curl_errorno cURL错误编号
+     * @param int $code 异常错误编号
+     * @param \Exception|null $previous
+     */
+    public function __construct($message = '', $curl_errorno = 0, $code = 0, \Exception $previous = null) {
+        $this->curl_errorno = $curl_errorno;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * 获取cURL错误信息
+     * @return NULL|string
+     */
+    public function curl_strerror() {
+        return function_exists('curl_strerror') ? curl_strerror($this->curl_errorno) : '';
+    }
+
+}
